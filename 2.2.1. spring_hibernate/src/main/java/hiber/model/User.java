82b9203b4bc6e62c05433dc2969a3b4be5c6f58e
Emile <emabish42@gmail.com>
2023-04-18 08:25:43 +0200

    Создал связь one-to-one и связал с сущностью Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,61 +1,81 @@
 package hiber.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private Car car;
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    public User() {
+    }
 
-   @Column(name = "name")
-   private String firstName;
+    public User(String firstName, String lastName, String email, Car car) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.car = car;
+    }
 
-   @Column(name = "last_name")
-   private String lastName;
+    public Long getId() {
+        return id;
+    }
 
-   @Column(name = "email")
-   private String email;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public Car getCar() {
+        return car;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public void setCar(Car car) {
+        this.car = car;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    @Override
+    public String toString() {
+        return "User{" +
+               "id=" + id +
+               ", firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", email='" + email + '\'' +
+               ", car=" + car +
+               '}';
+    }
 }
