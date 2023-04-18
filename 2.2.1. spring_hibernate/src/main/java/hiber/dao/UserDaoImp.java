490f76a5de2d5d4ac57fd34034e9524e11afc48f
Emile <emabish42@gmail.com>
2023-04-18 08:30:08 +0200

    Переопределил метод и написал hql-запрос, согласно заданию, поменял deprecated методы

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,25 +5,32 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
+    private final SessionFactory sessionFactory;
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().persist(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public List<User> listUsers() {
+        return sessionFactory.getCurrentSession().createQuery("select u from User u", User.class).list();
+    }
 
+    @Override
+    public User findUserByCar(String model, int series) {
+        return sessionFactory.getCurrentSession().createQuery(" select u from Car c join c.user u " +
+                                                              "where c.model=:model and c.series=:series", User.class)
+                .setParameter("model", model)
+                .setParameter("series", series).getSingleResultOrNull();
+    }
 }
